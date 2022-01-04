97dde9bcf25b4b6d3d8e3a85342edac3d5ed8404
Davisampaiom <dsampaiom@outlook.com>
2022-01-04 09:41:52 -0300

    primeiro commit

---
+++ src/components/UserCircle/index.tsx
@@ -0,0 +1,16 @@
+import {CircleContainer} from './styles';
+
+
+interface UserInfo {
+    initials: string;
+}
+
+const UserCircle = ({initials}: UserInfo) => {
+    return (
+        <CircleContainer>
+            DS
+        </CircleContainer>
+    )
+}
+
+export default UserCircle
